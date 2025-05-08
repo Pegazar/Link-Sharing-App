42e0bff6c35d0387d08f8ecbe7f584431f54e3e5
Pegazar <yusif.feyzullazade@gmail.com>
2025-05-08 14:10:34 +0400

    Editor Layout

--- src/layout/EditorLayout.jsx
+++ src/layout/EditorLayout.jsx
@@ -13,7 +13,7 @@ const EditorLayout = () => {
   const [savedLinks, setSavedLinks] = useState([]);
 
   return (
-    <div className="min-h-screen container mx-auto flex flex-col gap-4 p-4">
+    <div className="min-h-full xl:min-h-screen container mx-auto flex flex-col gap-4 p-4">
       <Navbar />
       <div className="lg:flex flex-col lg:flex-row gap-4 flex-1 min-h-0">
         <div className="w-1/3 xl:flex-shrink-0 hidden xl:block">
